a69e2b2f2de0c83b92344fdc022a822cd3e8adbe
robsv <svirskasr@janelia.hhmi.org>
2021-07-12 10:23:19 -0400

    Started adding Mongo integration

--- bin/copy_ppp_imagery.py
+++ bin/copy_ppp_imagery.py
@@ -336,7 +336,7 @@ def copy_files():
     for path in tqdm(json_files):
         parallel.append(dask.delayed(handle_single_json_file)(path))
         break
-    print("Copying %sPNGs" % ('and uploading ' if ARG.AWS else ''))
+    print("Copying and uploading %s body IDs"
     with ProgressBar():
         dask.compute(*parallel)
 
