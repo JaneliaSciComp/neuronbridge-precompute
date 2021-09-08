8d8e5978b17b00d87379d9bd1ae972247278b9f8
robsv <svirskasr@janelia.hhmi.org>
2021-09-08 17:28:17 -0400

    New table definition

--- dynamodb/create_janelia-neuronbridge-published.sh
+++ dynamodb/create_janelia-neuronbridge-published.sh
@@ -1,6 +1,6 @@
 aws dynamodb create-table \
-    --table-name janelia-neuronbridge-published\
-    --attribute-definitions AttributeName=key,AttributeType=S AttributeName=keyType,AttributeType=S \
-    --key-schema AttributeName=key,KeyType=HASH AttributeName=keyType,KeyType=RANGE \
-    --provisioned-throughput ReadCapacityUnits=200,WriteCapacityUnits=200 \
+    --table-name janelia-neuronbridge-published-prod\
+    --attribute-definitions AttributeName=itemType,AttributeType=S AttributeName=searchKey,AttributeType=S \
+    --key-schema AttributeName=itemType,KeyType=HASH AttributeName=searchKey,KeyType=RANGE \
+    --provisioned-throughput ReadCapacityUnits=50,WriteCapacityUnits=50 \
     --tags Key=PROJECT,Value=NeuronBridge Key=DEVELOPER,Value=svirskasr Key=STAGE,Value=prod
