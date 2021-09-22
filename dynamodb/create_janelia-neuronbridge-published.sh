2655d19956e66a3d4e652550f587cef47f769cc1
robsv <svirskasr@janelia.hhmi.org>
2021-09-22 11:00:15 -0400

    Added version

--- dynamodb/create_janelia-neuronbridge-published.sh
+++ dynamodb/create_janelia-neuronbridge-published.sh
@@ -1,5 +1,13 @@
+if [ $@ ]
+then
+  VERSION=$1
+else
+  echo "You must specify a version"
+  exit
+fi
+echo "Creating janelia-neuronbridge-published-${VERSION}"
 aws dynamodb create-table \
-    --table-name janelia-neuronbridge-published-prod\
+    --table-name janelia-neuronbridge-published-${VERSION}\
     --attribute-definitions AttributeName=itemType,AttributeType=S AttributeName=searchKey,AttributeType=S \
     --key-schema AttributeName=itemType,KeyType=HASH AttributeName=searchKey,KeyType=RANGE \
     --provisioned-throughput ReadCapacityUnits=50,WriteCapacityUnits=50 \
