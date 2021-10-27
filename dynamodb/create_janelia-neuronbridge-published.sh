7be0d03e6c220d4ffaf4be4d889e5cc660a84f3d
robsv <svirskasr@janelia.hhmi.org>
2021-10-27 15:55:44 -0400

    Changed provisioning

--- dynamodb/create_janelia-neuronbridge-published.sh
+++ dynamodb/create_janelia-neuronbridge-published.sh
@@ -10,5 +10,8 @@ aws dynamodb create-table \
     --table-name janelia-neuronbridge-published-${VERSION}\
     --attribute-definitions AttributeName=itemType,AttributeType=S AttributeName=searchKey,AttributeType=S \
     --key-schema AttributeName=itemType,KeyType=HASH AttributeName=searchKey,KeyType=RANGE \
-    --provisioned-throughput ReadCapacityUnits=50,WriteCapacityUnits=50 \
+    --billing-mode PAY_PER_REQUEST \
     --tags Key=PROJECT,Value=NeuronBridge Key=DEVELOPER,Value=svirskasr Key=STAGE,Value=prod
+
+# Formerly used PROVISIONED mode
+#--provisioned-throughput ReadCapacityUnits=50,WriteCapacityUnits=50 \
