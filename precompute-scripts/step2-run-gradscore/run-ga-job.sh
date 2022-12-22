04f422fe201d6bb74b323008e1e87f6ca9d835de
Cristian Goina <goinac@hhmi.org>
2022-12-22 17:34:02 -0500

    Started the grad score scripts

---
+++ precompute-scripts/step2-run-gradscore/run-ga-job.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+function run_ga_job {
+    # job index is 1-based index that should be passed either as the first arg
+    # or in the LSB_JOBINDEX env var
+    declare -i one_based_job_index=$1
+    # convert the 1-based index to 0-based
+    declare -i job_index=$((one_based_job_index - 1))
+    
+    masks_partition_index=$((job_index / MASKS_PER_JOB))
+    targets_partition_index=$((job_index % MASKS_PER_JOB))
+
+    masks_offset=$((masks_partition_index * MASKS_PER_JOB))
+    targets_offset=$((targets_partition_index * TARGETS_PER_JOB))
+
+    if [[ -n ${CDS_TAG} ]]; then
+        PROCESS_TAG_ARG="--processing-tag ${CDS_TAG}"
+    else
+        PROCESS_TAG_ARG=""
+    fi
+
+    case ${ALIGNMENT_SPACE} in
+        JRC2018_Unisex_20x_HR|JRC2018_VNC_Unisex_40x_DS)
+            AS_ARG="-as ${ALIGNMENT_SPACE}"
+            ;;
+        *)
+            echo "Invalid alignment space: ${ALIGNMENT_SPACE} - no alignment space argument will be used"
+            AS_ARG=""
+            ;;
+    esac
+
+    if [[ -n ${DB_CONFIG} && -f ${DB_CONFIG} ]]; then
+        CONFIG_ARG="--config ${DB_CONFIG}"
+    else
+        echo "No database configuration set or found! Will use a default local database"
+        CONFIG_ARG=""
+    fi
+
+    MASKS_ARG="-m ${MASKS_LIBRARY}:${masks_offset}:${MASKS_PER_JOB}"
+    TARGETS_ARG="-i ${TARGETS_LIBRARY}:${targets_offset}:${TARGETS_PER_JOB}"
+
+    cds_cmd="${JAVA_EXEC} \
+        ${JAVA_OPTS} ${JAVA_MEM_OPTS} ${JAVA_GC_OPTS} \
+        -jar ${NEURONSEARCH_TOOLS_JAR} \
+        gradientScores \
+        ${CONFIG_ARG} \
+        ${PROCESS_TAG_ARG} \
+        ${AS_ARG} \
+        ${MASKS_ARG} \
+        ${TARGETS_ARG}
+        --mirrorMask \
+        --dataThreshold ${MASK_THRESHOLD} \
+        --maskThreshold ${DATA_THRESHOLD} \
+        --pixColorFluctuation ${PIX_FLUCTUATION} \
+        --xyShift ${XY_SHIFT} \
+        --pctPositivePixels ${PIX_PCT_MATCH} \
+        -ps ${PROCESSING_PARTITION_SIZE} \
+        "
+    echo "$HOSTNAME $(date):> ${cds_cmd}"
+    ($cds_cmd)
+}
+
+job_index=$((${LSB_JOBINDEX:-$1}))
+output_log=${JOB_LOGPREFIX}/ga_${job_index}.log
+echo "$(date) Run Job ${job_index} (Output log: ${output_log})"
+run_ga_job ${job_index} > ${output_log} 2>&1
---
+++ precompute-scripts/step2-run-gradscore/submit-ga.sh
@@ -0,0 +1,43 @@
+anatomical_area=$1
+
+# Precompute tools location
+SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+
+source "${SCRIPT_DIR}/../global-params.sh" ${anatomical_area}
+source "${SCRIPT_DIR}/ga-params.sh"
+
+function localRun {
+    if [[ $# -lt 2 ]] ; then
+      echo "localRun <from> <to>"
+            exit 1
+    fi
+    declare -i from=$1
+    declare -i to=$2
+    for ((currindex=${from}; currindex<=${to}; currindex++)) ; do
+        ${SCRIPT_DIR}/run-ga-job.sh ${currindex}
+    done
+}
+
+function gridRun {
+    if [[ $# -lt 2 ]] ; then
+      echo "gridRun <from> <to>"
+            exit 1
+    fi
+    declare -i from=$1
+    declare -i to=$2
+    bsub -n ${CORES_RESOURCE} \
+        -J GRADSCORE[${from}-${to}] \
+        -P ${CLUSTER_PROJECT_CODE} \
+        ${OTHER_BSUB_OPTIONS} \
+        ${SCRIPT_DIR}/run-ga-job.sh
+}
+
+echo "Total jobs: $TOTAL_JOBS"
+
+mkdir -p $JOB_LOGPREFIX
+
+# to run locally use localRun <from> <to>
+# to run on the grid use gridRun <from> <to>
+startcmd="${RUN_CMD} ${FIRST_JOB} ${LAST_JOB}"
+echo $startcmd
+($startcmd)
