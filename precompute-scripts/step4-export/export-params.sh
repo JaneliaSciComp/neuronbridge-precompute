0c2049105528b5c1656b1eda8ab0140baeee05ed
Cristian Goina <goinac@hhmi.org>
2023-02-08 14:33:40 -0500

    exoport brain and vnc stores

--- precompute-scripts/step4-export/export-params.sh
+++ precompute-scripts/step4-export/export-params.sh
@@ -1,18 +1,12 @@
 BASE_EXPORT_FOLDER=${BASE_EXPORT_FOLDER:=/nrs/neuronbridge}
 DATA_VERSION=${DATA_VERSION:=v3.0.0}
 
-OUTPUT_DIR=${EXPORT_DIR:=${BASE_EXPORT_FOLDER}/${DATA_VERSION}}
-JACS_URL=${JACS_URL:="http://<host>/api/rest-v2"}
-JACS_AUTH=${JACS_AUTH:="APIKEY thekeyvalue"}
+export OUTPUT_DIR=${EXPORT_DIR:=${BASE_EXPORT_FOLDER}/${DATA_VERSION}}
+export JACS_URL=${JACS_URL:="http://<host>/api/rest-v2"}
+export JACS_AUTH=${JACS_AUTH:="APIKEY thekeyvalue"}
 
-BRAIN_STORE=fl:open_data:brain
-VNC_STORE=fl:pre_release:vnc
+export BRAIN_STORE=fl:open_data:brain
+export VNC_STORE=fl:pre_release:vnc
 
-JOB_LOGPREFIX=${JOB_LOGPREFIX:=${LOGS_DIR}/exports}
-READ_BATCH_SIZE=2000
-
-export OUTPUT_DIR
-export JACS_URL
-export JACS_AUTH
-export JOB_LOGPREFIX
-export READ_BATCH_SIZE
+export JOB_LOGPREFIX=${JOB_LOGPREFIX:=${LOGS_DIR}/exports}
+export READ_BATCH_SIZE=2000
