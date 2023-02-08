f32a1d7e0b6ccf53d72fcc7f4ccfa2a786ad5629
Cristian Goina <goinac@janelia.hhmi.org>
2023-02-08 13:04:31 -0500

    read area from command line

--- precompute-scripts/step4-export/submit-export.sh
+++ precompute-scripts/step4-export/submit-export.sh
@@ -2,6 +2,14 @@
 
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 
+# AREA: brain, vnc, brain+vnc, vnc+brain
+AREA=${AREA:=$1}
+
+if [ "$#" -ge 1 ]; then
+    shift
+fi
+
+EXPORT_TYPE=${EXPORT_TYPE:=$1}
 
 sh ${SCRIPT_DIR}/export-job.sh brain+vnc EM_MIPS
 sh ${SCRIPT_DIR}/export-job.sh brain+vnc LM_MIPS
