053fa6c34d0242a33da20041508923e3a51895f2
Rob Svirskas <robsvi@gmail.com>
2023-05-05 13:54:37 -0400

    Added bucket stats

--- bin/partition_check.py
+++ bin/partition_check.py
@@ -1,10 +1,9 @@
-''' partition_check.py
-    Find the last partition for every NeuronBridge library on AWS S3
+''' cdm_bucket_status.py
+    Show status for every NeuronBridge library on AWS S3
 '''
 import json
-import sys
 import boto3
-from aws_s3_lib import get_prefixes
+from aws_s3_lib import bucket_stats, get_prefixes
 
 def read_object(bucket, key):
     ''' Read a "counts_denormalized" object and return the number of objects
@@ -16,7 +15,7 @@ def read_object(bucket, key):
     '''
     try:
         data = S3.get_object(Bucket=bucket, Key=key)
-    except Exception:
+    except Exception: # pylint: disable=W0718
         return 0
     contents = data['Body'].read().decode("utf-8")
     data = json.loads(contents)
@@ -33,18 +32,18 @@ def process_template(bucket, template):
         Returns:
           None
     """
-    manifold = bucket.split("-")[-1]
-    if manifold == "depth":
-        manifold = "prod"
     libraries = get_prefixes(bucket, template)
     for library in libraries:
         if "Test" in library:
             continue
-        images = read_object(bucket, "/".join([template, library, "counts_denormalized.json"]))
-        parts = get_prefixes(bucket, "/".join([template, library, "searchable_neurons"]))
+        images = read_object(bucket, "/".join([template, library,
+                                               "counts_denormalized.json"]))
+        parts = get_prefixes(bucket, "/".join([template, library,
+                                               "searchable_neurons"]))
         if parts:
             neurons = read_object(bucket, "/".join([template, library,
-                                            "searchable_neurons", "counts_denormalized.json"]))
+                                            "searchable_neurons",
+                                            "counts_denormalized.json"]))
             arr = []
             for part in parts:
                 if part.isnumeric():
@@ -53,7 +52,7 @@ def process_template(bucket, template):
             last = arr[-1]
         else:
             last = neurons = ""
-        print(f"{manifold:<8}  {template:<25}  {library:<34}  {images:>6}  {neurons:>7}  {last:>4}")
+        print(f"{template:<25}  {library:<34}  {images:>6}  {neurons:>7}  {last:>4}")
 
 
 def process_manifold(bucket):
@@ -70,16 +69,37 @@ def process_manifold(bucket):
         process_template(bucket, template)
 
 
+def humansize(num, suffix='B'):
+    ''' Return a human-readable storage size
+        Keyword arguments:
+          num: size
+          suffix: default suffix
+        Returns:
+          string
+    '''
+    for unit in ['', 'K', 'M', 'G', 'T']:
+        if abs(num) < 1024.0:
+            return f"{num:.1f}{unit}{suffix}"
+        num /= 1024.0
+    return "{num:.1f}P{suffix}"
+
+
 def process_buckets():
-    """ Process all NeuronBridge cCDM buckets
+    """ Process all NeuronBridge CDM buckets
         Keyword arguments:
           None
         Returns:
           None
     """
-    print(f"{'Manifold':<8}  {'Template':<25}  {'Library':<34}  {'Images':>6}  {'Neurons':>7}  {'Part':>4}")
     for suffix in ("-dev", "-devpre", "-prodpre", ""):
-        process_manifold("janelia-flylight-color-depth" + suffix)
+        name = "janelia-flylight-color-depth" + suffix
+        bstat = bucket_stats(bucket=name, profile="" if suffix else "FlyLightPDSAdmin")
+        if not bstat['objects']:
+            continue
+        print(f"\n{name}: {bstat['objects']:,} objects, {humansize(bstat['size'])}")
+        print(f"{'Template':<25}  {'Library':<34}  {'Images':>6}  {'Neurons':>7}  " \
+              + f"{'Part':>4}")
+        process_manifold(name)
 
 
 if __name__ == '__main__':
