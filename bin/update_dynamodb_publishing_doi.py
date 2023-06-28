a3516dec39b3b798b151820eeafe408859a3d6a7
Rob Svirskas <robsvi@gmail.com>
2023-06-28 16:09:04 -0400

    Added error check for key not found

--- bin/update_dynamodb_publishing_doi.py
+++ bin/update_dynamodb_publishing_doi.py
@@ -201,6 +201,8 @@ def get_citation(doi):
     """
     if doi not in CITATION:
         first = []
+        if doi not in DOI:
+            LOGGER.error("%s is not a known DOI", doi)
         for auth in DOI[doi]["author"]:
             if auth["sequence"] == "first":
                 first.append(auth["family"])
