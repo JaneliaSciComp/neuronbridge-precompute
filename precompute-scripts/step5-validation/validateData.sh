48bc1957452627f899fce12b783630b0ad7e2772
Cristian Goina <goinac@janelia.hhmi.org>
2024-02-22 13:16:01 -0500

    updated cds tools

--- precompute-scripts/step5-validation/validateData.sh
+++ precompute-scripts/step5-validation/validateData.sh
@@ -4,7 +4,10 @@ JAR_VERSION=3.1.0
 
 ALIGNMENT_SPACE=JRC2018_VNC_Unisex_40x_DS
 LIBNAME=flylight_split_gal4_published
-VALIDATED_NAMES_ARG=
+
+AS_ARG=
+LIB_ARG=
+VALIDATED_SAMPLES_ARG=
 APPLY_ERROR_TO_LMCDS="--apply-error-tag-to-lm-cdmatches"
 APPLY_ERROR_TO_EMCDS="--apply-error-tag-to-em-cdmatches"
 ERROR_TAG_ARG="--error-tag validationError ${APPLY_ERROR_TO_LMCDS} ${APPLY_ERROR_TO_EMCDS}"
@@ -18,15 +21,15 @@ DEBUG_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006"
 $RUNNER java \
     -Xmx270G -Xms270G \
     ${DEBUG_OPTS} \
-    -jar target/colormipsearch-${JAR_VERSION}-jar-with-dependencies.jar \
+    -jar ../neuron-search-tools/target/colormipsearch-${JAR_VERSION}-jar-with-dependencies.jar \
     validateDBData \
     ${PROD_CONFIG} \
-    -as ${ALIGNMENT_SPACE} \
     --jacs-url http://e03u04.int.janelia.org:8800/api/rest-v2 \
     --authorization "APIKEY MyKey" \
-    -l ${LIBNAME} \
+    ${AS_ARG} \
+    ${LIB_ARG} \
     ${EXCLUDED_LIBRARIES_ARG} \
-    ${VALIDATED_NAMES_ARG} \
+    ${VALIDATED_SAMPLES_ARG} \
     ${ERROR_TAG_ARG} \
     --read-batch-size 2000 \
     -ps 50 \
