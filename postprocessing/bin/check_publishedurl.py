19cdef55ec6d5cebe6533bad4fc3d6fec27b0ef5
Rob Svirskas <robsvi@gmail.com>
2025-10-14 16:12:30 -0400

    Updated update_dynamodb_published_versioned.py to create fully-qualified body IDs in published versioned table Fixed version generation in update_dynamodb_publishing_doi.py Improved reporting in check_publishedurl.py Improved error handling in bucket_status.py

--- postprocessing/bin/check_publishedurl.py
+++ postprocessing/bin/check_publishedurl.py
@@ -3,6 +3,7 @@
 
 import argparse
 import collections
+from copy import deepcopy
 import json
 from operator import attrgetter
 import sys
@@ -19,9 +20,14 @@ AWSS3 = {"client": None, "resource": None}
 MANIFEST = {}
 # Database
 DB = {}
+# Known NeuronBridge slide codes
+SLIDE_CODES = {}
 # Counters
 COUNT = collections.defaultdict(lambda: 0, {})
+CHECKED = collections.defaultdict(lambda: 0, {})
 FOUND = collections.defaultdict(lambda: 0, {})
+# Output
+MISSING = []
 
 def terminate_program(msg=None):
     ''' Terminate the program gracefully
@@ -74,8 +80,15 @@ def get_parms():
         Returns:
             None
     """
+    if ARG.MANIFEST:
+        with open(ARG.MANIFEST, 'r', encoding='ascii') as instream:
+            for line in instream:
+                MANIFEST['/'.join([ARG.BUCKET, line.strip()])] = True
+        print(f"Manifest entries:          {len(MANIFEST):,}")
+    if ARG.COLLECTION == 'publishedLMImage':
+        return
     if not ARG.TEMPLATE:
-        ARG.TEMPLATE = NB.get_template(AWSS3["client"], ARG.BUCKET)
+        ARG.TEMPLATE = NB.get_template(AWSS3["client"], 'janelia-flylight-color-depth')
         if not ARG.TEMPLATE:
             terminate_program("No alignment template selected")
     coll = DB['neuronbridge'].publishedURL
@@ -107,12 +120,7 @@ def get_parms():
         terminate_program("No NeuronBridge data version was found")
     print(f"Template:                  {ARG.TEMPLATE}")
     print(f"Library:                   {ARG.LIBRARY}")
-    print(f"NeuronBridge data version: {ARG.VERSION}")
-    if ARG.MANIFEST:
-        with open(ARG.MANIFEST, 'r', encoding='ascii') as instream:
-            for line in instream:
-                MANIFEST['/'.join([ARG.BUCKET, line.strip()])] = True
-        print(f"Manifest entries:          {len(MANIFEST):,}")        
+    print(f"NeuronBridge data version: {ARG.VERSION}")        
 
 
 def initialize_program():
@@ -134,15 +142,32 @@ def initialize_program():
             DB[source] = JRC.connect_database(dbo)
         except Exception as err:
             terminate_program(err)
+    try:
+        rows = DB['neuronbridge'].publishedURL.distinct('slideCode')
+    except Exception as err:
+        terminate_program(err)
+    for row in rows:
+        SLIDE_CODES[row] = True
+    LOGGER.info(f"Known NeuronBridge slide codes: {len(SLIDE_CODES):,}")
     initialize_s3()
     get_parms()
 
 
 def compare_manifest(bucket, obj, objtype, row):
+    LOGGER.debug(f"Comparing {objtype} {bucket}/{obj} for {row['_id']}")
     if '/'.join([bucket, obj]) in MANIFEST:
         FOUND[objtype] += 1
     else:
         LOGGER.warning(f"Missing {objtype} {bucket}/{obj} for {row['_id']}")
+        MISSING.append(f"{row['slideCode']}\t{row['releaseName']}\t{bucket}/{obj}\n")
+        if ARG.DELETE and ARG.COLLECTION == 'publishedLMImage':
+            coll = DB['neuronbridge'].publishedLMImage
+            payload = deepcopy(row['files'])
+            del payload[objtype]
+            if not payload:
+                coll.delete_one({"_id": row['_id']})
+            else:
+                coll.update_one({"_id": row['_id']}, {"$set": {"files": payload}})
 
 
 def compare_s3(bucket, obj, objtype, row):
@@ -159,7 +184,13 @@ def compare_s3(bucket, obj, objtype, row):
     FOUND[objtype] += 1
 
 
-def processing():
+def processing_publishedurl():
+    """ Process publishedURL
+        Keyword arguments:
+          None
+        Returns:
+          None
+    """
     coll = DB['neuronbridge'].publishedURL
     payload = {"alignmentSpace": ARG.TEMPLATE,
                "libraryName": ARG.LIBRARY,
@@ -169,8 +200,7 @@ def processing():
         rows = coll.find(payload)
     except Exception as err:     
         terminate_program(err)
-    print(f"Processing {cnt:,} documents")
-    checked = collections.defaultdict(lambda: 0, {})
+    print(f"Processing {cnt:,} documents from {ARG.COLLECTION}")
     for row in tqdm(rows, total=cnt):
         for objtype, objkey in row['uploaded'].items():
             objkey = objkey.replace("https://s3.amazonaws.com/", "")
@@ -181,25 +211,61 @@ def processing():
                 compare_manifest(bucket, obj, objtype, row)
             else:
                 compare_s3(bucket, obj, objtype, row)
-            checked[objtype] += 1
-    print(f"Checked {sum(checked.values()):,} images")
-    for objtype, cnt in checked.items():
-        print(f"{objtype + ':':19} {cnt:,}")
-    print(f"Found {sum(FOUND.values()):,} objects")
-    for objtype, cnt in FOUND.items():
-        print(f"{objtype + ':':19} {cnt:,}")
+            CHECKED[objtype] += 1
+
+
+def processing_publishedlmimage():
+    """ Process publishedLMImage
+        Keyword arguments:
+          None
+        Returns:
+          None
+    """
+    coll = DB['neuronbridge'].publishedLMImage
+    payload = {}
+    if ARG.RELEASE:
+        payload['releaseName'] = ARG.RELEASE
+    if ARG.SLIDE:
+        payload['slideCode'] = ARG.SLIDE
+    try:
+        cnt = coll.count_documents(payload)
+        rows = coll.find(payload)
+    except Exception as err:
+        terminate_program(err)
+    print(f"Processing {cnt:,} documents from {ARG.COLLECTION}")
+    for row in tqdm(rows, total=cnt):
+        if row['slideCode'] not in SLIDE_CODES:
+            continue
+        for objtype, objkey in row['files'].items():
+            objkey = objkey.replace("https://s3.amazonaws.com/", "")
+            bucket, obj = objkey.split('/', 1)
+            obj = obj.replace("+", "%20")
+            if ARG.MANIFEST:
+                if bucket != ARG.BUCKET:
+                    continue
+                compare_manifest(bucket, obj, objtype, row)
+            else:
+                compare_s3(bucket, obj, objtype, row)
+            CHECKED[objtype] += 1
 
 # -----------------------------------------------------------------------------
 
 if __name__ == '__main__':
     PARSER = argparse.ArgumentParser(
         description="Add ORCID information to MongoDB:orcid")
+    PARSER.add_argument('--collection', dest='COLLECTION', action='store',
+                        default='publishedURL', choices=['publishedURL', 'publishedLMImage'],
+                        help='MongoDB collection')
     PARSER.add_argument('--bucket', dest='BUCKET', action='store',
                         default='janelia-flylight-color-depth', help='AWS S3 bucket')
     PARSER.add_argument('--template', dest='TEMPLATE', action='store',
                         help='Template')
     PARSER.add_argument('--library', dest='LIBRARY', action='store',
                         default='', help='Library')
+    PARSER.add_argument('--release', dest='RELEASE', action='store',
+                        help='Release name')
+    PARSER.add_argument('--slide', dest='SLIDE', action='store',
+                        help='Slide code')
     PARSER.add_argument('--manifold', dest='MANIFOLD', action='store',
                         default='prod', choices=['dev', 'devpre', 'prod', 'prodpre'],
                         help='S3 manifold')
@@ -207,6 +273,8 @@ if __name__ == '__main__':
                         help='NeuronBridge data version')
     PARSER.add_argument('--manifest', dest='MANIFEST', action='store',
                         help='Search manifest instead of AWS S3')
+    PARSER.add_argument('--delete', dest='DELETE', action='store_true',
+                        default=False, help='Delete missing file pointers from publishedLMImage')
     PARSER.add_argument('--verbose', dest='VERBOSE', action='store_true',
                         default=False, help='Flag, Chatty')
     PARSER.add_argument('--debug', dest='DEBUG', action='store_true',
@@ -214,5 +282,14 @@ if __name__ == '__main__':
     ARG = PARSER.parse_args()
     LOGGER = JRC.setup_logging(ARG)
     initialize_program()
-    processing()
+    processing_publishedurl() if ARG.COLLECTION == 'publishedURL' else processing_publishedlmimage()
+    print(f"Checked {sum(CHECKED.values()):,} images")
+    for objtype, cnt in CHECKED.items():
+        print(f"{objtype + ':':22} {cnt:,}")
+    print(f"Found {sum(FOUND.values()):,} objects")
+    for objtype, cnt in FOUND.items():
+        print(f"{objtype + ':':22} {cnt:,} ({cnt/CHECKED[objtype]*100:.2f}%)")
+    if MISSING:
+        with open(f"{ARG.COLLECTION}_missing.txt", 'w', encoding='ascii') as outfile:
+            outfile.writelines(MISSING)
     terminate_program()
